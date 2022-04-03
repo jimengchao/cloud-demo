99bfab26fedd4e72fb1205a78844b4bc6ee0e45a
muluo <mengchao1027@gmail.com>
2022-04-03 21:20:09 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 
 export default function App() :JSX.Element {
+    let url: string = process.env.NODE_ENV === 'production' ? 'https://cloudflare-demo.mengchao10271254.workers.dev' : '/api'
 
-  const [ data, setData ] = useState<string>('');
+    const [ data, setData ] = useState<string>('');
+    const [ text, setText ] = useState<string>('');  
+
+  useEffect(() => {
+    fetch(`${url}/get${window.location.search}`, {
+    }).then(res => res.text()).then(res => setText(res));
+  }, [])
+  
 
   function handleClick():void {
-    let url: string = process.env.NODE_ENV === 'production' ? 'https://cloudflare-demo.mengchao10271254.workers.dev' : '/api'
-    fetch(`${url}${window.location.search}`).then(res => res.text()).then(res => setData(res));
+    fetch(`${url}/post${window.location.search}`, {
+      method: "POST",
+    }).then(res => res.text()).then(res => setData(res));
     console.log('You clicked me');
   }
 
@@ -19,8 +28,11 @@ export default function App() :JSX.Element {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <button onClick={handleClick}>请求Serverless</button>
-        请求来的数据 ： { data }
+        请求来的数据 ： { data } <br />
+        content: { text }
+
       </header>
+
     </div>
   );
 }
